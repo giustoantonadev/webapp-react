48a224c6cd7752665f570a191cb83d0974a8f05d
giustoantonadev <giustoantonadev@gmail.com>
2026-04-21 14:06:32 +0200

    Add API integration for movie data retrieval in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+import api from "../api/api";
+import MovieList from '../components/MovieList'
+
 export default function Home() {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        api.get('/movies')
+            .then(res => setMovies(res.data))
+            .catch(err => console.error('Errore caricamento film:', err))
+    }, [])
     return (
         <>
             <h2>Lista Film</h2>
-            <p>Qui apparirà lista dei film</p>
+            <MovieList movies={movies} />
         </>
     )
 }
